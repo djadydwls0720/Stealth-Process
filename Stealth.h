ed5cb5909014b2549267954efe549fa5b0738edc
djadydwls0720 <78453558+djadydwls0720@users.noreply.github.com>
2024-08-08 09:47:32 +0900

    Delete Stealth.h

--- Stealth.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdio.h>
-#include <Windows.h>
-#include <winternl.h>
-#include <string.h>
-void Stealth();
-
-#define NewNtQuerySystemInformation_size  (ULONGLONG)NewNtQuerySystemInformation - (ULONGLONG)DumyFunc;
-#define Lower(s1) s1 >= 65 && s1 <= 90 ? (wchar_t)s1 + 32 : s1
